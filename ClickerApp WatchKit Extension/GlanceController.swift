b0a1bd97323251b574a4babd220939b8821b8e21
Chris Jimenez <cjimenez16@gmail.com>
2016-03-18 00:53:00 -0600

    Add Glance functionality

--- ClickerApp WatchKit Extension/GlanceController.swift
+++ ClickerApp WatchKit Extension/GlanceController.swift
@@ -8,12 +8,43 @@
 
 import WatchKit
 import Foundation
+import RxSwift
 
 
 class GlanceController: WKInterfaceController {
+    
+    @IBOutlet var titleLabel: WKInterfaceLabel!
+    
+    @IBOutlet var descriptionLabel: WKInterfaceLabel!
+    
+    @IBOutlet var countLabel: WKInterfaceLabel!
+    
+    var disposeBag = DisposeBag()
+    
+    var viewModel: ClickerViewModel = ClickerViewModel()
+    
+    var settingsViewModel: SettingsViewModel = SettingsViewModel()
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
+        
+        viewModel.clickerCountDriver?
+            .map { String($0) }
+            .driveNext{ [weak self] count in
+                self?.countLabel.setText(count)
+            }.addDisposableTo(disposeBag)
+        
+        
+        settingsViewModel.settingsChangedSubject
+            .subscribeNext{ [weak self] settings in
+                
+                self?.titleLabel.setTextColor(settings.color.uiColor)
+                
+                self?.countLabel.setTextColor(settings.color.uiColor)
+                
+                
+            }.addDisposableTo(disposeBag)
+
         
         // Configure interface objects here.
     }
@@ -21,6 +52,11 @@ class GlanceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        
+        viewModel.updateToLatestClicker()
+        
+        settingsViewModel.getLatestSettings()
     }
 
     override func didDeactivate() {
