9518daabd4922a8d12e605e7895b5b5a9c9656f2
Christopher Jimenez <cjimenez16@gmail.com>
2016-03-21 17:48:48 -0600

    Add animation when the counter changes

--- ClickerApp WatchKit Extension/InterfaceController.swift
+++ ClickerApp WatchKit Extension/InterfaceController.swift
@@ -31,24 +31,42 @@ class InterfaceController: WKInterfaceController {
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
+        /**
+        *  Observes the clicker count, maps it to a String and trive the label
+        */
         viewModel.clickerCountDriver?
                     .map { String($0) }
                     .driveNext{ [weak self] count in
-                        self?.clickerCountLabel.setText(count)
+                        
+                        self?.animateWithDuration(0.35, animations: { () -> Void in
+                        
+                            self?.clickerCountLabel.setText(count)
+                        });
+                        
                     }.addDisposableTo(disposeBag)
         
         
+        /**
+        *  Observes the settings change, update the multiplier and the background color
+        */
         settingsViewModel.settingsChangedSubject
                          .subscribeNext{ [weak self] settings in
                             
-                    self?.backgroundContainer.setBackgroundColor(settings.color.uiColor)
+                        //Add an animation for the transition
+                        self?.animateWithDuration(1, animations: { () -> Void in
+                                
+                            self?.backgroundContainer.setBackgroundColor(settings.color.uiColor)
+                        });
                             
                     self?.incrementMultiplier = settings.incrementMultiplier
             
             
         }.addDisposableTo(disposeBag)
     }
-
+    
+    /**
+     Trigger when the view will activate
+     */
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
@@ -58,25 +76,34 @@ class InterfaceController: WKInterfaceController {
         settingsViewModel.getLatestSettings()
 
     }
-
+    
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
+    /**
+     Add to the counter when the option is clicked
+     */
     @IBAction func clickedAddTouched() {
         
         viewModel.incrementClikerByMultiplier(incrementMultiplier)
         
     }
     
-
+    /**
+     Reset the counter when the menu
+     */
     @IBAction func resetMenuTouched() {
         
         viewModel.resetClicker()
         
     }
     
+    /**
+     Substract the counter
+     */
     @IBAction func substractMenuTouched() {
         
         viewModel.decrementClikerByMultiplier(incrementMultiplier)
